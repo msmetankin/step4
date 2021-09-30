e4978168525d2a4b3fdab5402b59b65644c7b0c6
msmetankin <m.smetankin@dunice.net>
2021-09-30 16:52:09 +0300

    Initial commit

---
+++ step4/src/test/java/Dunice/step4/Step4ApplicationTests.java
@@ -0,0 +1,13 @@
+package Dunice.step4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
